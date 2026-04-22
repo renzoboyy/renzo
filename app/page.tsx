e4794b8863bd09d3a94dbb24e97098ef36cd68c5
renzoboyy <fatdonut380@gmail.com>
2026-04-22 20:14:23 +0800

    added bg

--- app/page.tsx
+++ app/page.tsx
@@ -3,21 +3,25 @@ import './globals.css';
 
 export default function Home() {
   return (
-    <main>
-      <h1>Hello world</h1>
 
-      <div className="h-screen w-full flex 
-      items-center justify-center bg-slate-600">
-        <div className="grid h-full w-full
-        grid-cols-4 grid-rows-3 gap-3 p-20
-        xl:m-64 lg:m-32 md:m-16">
-          <div className="col-span-1 row-span-1 bg-slate-900
-          rounded-xl"></div>
-          <div className="col-span-2 row-span-1 bg-slate-900
-          rounded-xl"></div>
+    <div className="h-screen w-full flex 
+    items-center justify-center">
+      <div className="grid h-full w-full
+      grid-cols-4 grid-rows-3 gap-3 p-20
+      ">
+        <div className="col-span-1 row-span-1 bg-gray-900
+        rounded-xl">
+          <div className="flex align-middle justify-center">
+            hello
+          </div>
         </div>
+        <div className="col-span-3 row-span-1 bg-gray-900
+        rounded-xl"></div>
+        <div className="col-span-2 row-span-1 bg-gray-900
+        rounded-xl"></div>
       </div>
-    </main>
+    </div>
+
   )
 }
 
