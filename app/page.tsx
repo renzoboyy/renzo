832f5eb1663b3270b887680838169cd064b5410e
renzoboyy <fatdonut380@gmail.com>
2026-04-23 10:00:12 +0800

    added github calendar (inspired by stimmie.dev)

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,72 @@
 import Image from "next/image";
+import { Inter, Poppins } from "next/font/google";
+import { GitHubCalendar } from 'react-github-calendar';
 import './globals.css';
+
+type ColorPalette = [string, string, string, string, string];
+
+interface ThemeInput {
+  light: ColorPalette;
+  dark: ColorPalette;
+}
+
+const explicitTheme: ThemeInput = {
+  light: ['#999999', '#997A7A', '#995454', '#992A2A', '#FF0000'],
+  dark: ['#383838', '#4D455D', '#7DB9B6', '#F5E9CF', '#E96479'],
+}
+
+const interFontBold = Inter({
+  subsets: ["latin"],
+  weight: "800", 
+})
+
+const poppins = Poppins({
+  weight: "300"
+})
 
 export default function Home() {
   return (
-
-    <div className="h-screen w-full flex 
-    items-center justify-center">
-      <div className="grid h-full w-full
-      grid-cols-4 grid-rows-3 gap-3 p-20
-      ">
-        <div className="col-span-1 row-span-1 bg-gray-900
-        rounded-xl">
-          <div className="flex align-middle justify-center">
-            hello
+    <main>
+      <div className="h-screen w-full flex 
+      items-center justify-center">
+        <div className="grid h-full w-full
+        grid-cols-4 grid-rows-3 gap-3 p-20
+        ">
+          <div className="col-span-1 row-span-1 bg-gray-900
+          rounded-xl">
+            <div className={`ml-4 mt-2 text-4xl ${interFontBold.className}`}>
+              <h1>renzo</h1>
+              <div className={`text-base opacity-50 ${poppins.className}`}>
+                Information Systems
+              </div>
+            </div>
+          </div>
+          <div className="col-span-3 row-span-1 bg-gray-900
+          rounded-xl"></div>
+          <div className="col-span-2 row-span-1 bg-gray-900
+          rounded-xl"></div>
+          <div className="col-span-2 row-span-1 bg-gray-900
+          rounded-xl">
+          </div>
+          <div className="col-span-1 row-span-1 bg-gray-900
+          rounded-xl">
+            <div className={`ml-4 mt-2 text-3xl ${interFontBold.className}`}>
+              GitHub
+              <div className={`mt-2 mr-4 text-lg ${poppins.className}`}>
+                <GitHubCalendar 
+                username="renzoboyy"
+                colorScheme="light" 
+                theme={explicitTheme}
+                labels={{
+                  totalCount: '{{count}} contributions in the last year',
+                }}
+                />
+              </div>
+            </div>
           </div>
         </div>
-        <div className="col-span-3 row-span-1 bg-gray-900
-        rounded-xl"></div>
-        <div className="col-span-2 row-span-1 bg-gray-900
-        rounded-xl"></div>
       </div>
-    </div>
-
+    </main>
   )
 }
 
